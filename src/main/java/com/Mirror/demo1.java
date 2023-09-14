94da463e84e05209f40f854b7a9735feb58981a0
Mirror <haciii1022@gmail.com>
2023-09-14 20:28:04 +0800

    test-branch 1st commit

--- src/main/java/com/Mirror/demo1.java
+++ src/main/java/com/Mirror/demo1.java
@@ -4,5 +4,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello haciii!");
+        System.out.println("test5");
     }
 }
