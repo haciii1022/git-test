fa588ffdc816bc8cf8cdfdfe25ebbe0106779c26
Mirror <haciii1022@gmail.com>
2023-09-14 20:04:27 +0800

    second commit

---
+++ src/main/java/com/Mirror/demo1.java
@@ -0,0 +1,8 @@
+package com.Mirror;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello haciii!");
+    }
+}
